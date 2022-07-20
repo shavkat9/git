ec844a65e28c417703d6804b43eb81723bbecde7
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-07-20 21:37:22 +0500

    hello
    next website

---
+++ Новая папка (2)/script.js
@@ -0,0 +1,101 @@
+// var empty = []
+// var raqamlar =[2,3,4,6,8] raqamli array
+// var music = [1.1, true, "a"]  3ta har xil element
+ 
+// var el = 1024
+
+// var table  = [el, el+1, el+2]  ozgaruvchilar bilan ishlash
+
+// var arrObj = [
+//     [1, {x:1, y: 2}],
+//     [2, {x:3, y: 4}]
+// ]
+
+// var arr = new Array();
+// var arr1 = new Array(10)
+// var arr2 = new Array(5,4,3, "test")
+
+
+// // 2 arrayga murojat qilish
+
+// console.log(raqamlar[1]);
+// console.log(raqamlar .length);
+// raqamlar[2] = '456'
+// console.log(raqamlar);
+// console.log(raqamlar);
+// raqamlar[raqamalr.length] = 'New element'
+// console.log(raqamlar);
+
+// // 3 Arrayni oxiriga malumot qo'shish
+// var arr =[]
+// console.log(arr);
+
+// arr.push('bir')
+// console.log(arr);
+// arr.push('ikki')
+// console.log(arr);
+
+// // 4 boshiga malumit qoshish
+
+// var arr2 = [1,2,3,4,5,6,7]
+
+// console.log(arr2);
+// arr2.unshift('bir')
+// console.log(arr2);
+
+// // 5. boshidan bitta elementni o'chirish.
+
+
+// arr2.shift()
+// console.log(arr2);
+
+// // 6. oxiridan bitta oldin ochiradi
+
+// arr2.pop()
+// console,log (arr2);
+
+// delete arr2(2)
+// console.log(arr2);
+
+
+// show ( data)
+
+
+// for (var xabar of data) {
+//     console.log(xabar);
+// }
+// function show(data){
+//     let news ="";
+
+//     for(let xabar of data) {
+//     news += 
+//     <div class="card">
+//     <h3>${xabar.name}</h3>
+//     <p>${xabar.info}</p>
+
+//   </div>
+// ;
+// }
+// document.getElementById("news").innerHTML = news
+
+// }
+
+
+
+// let ism = " Hello Shavkat Berdibekov"
+// alert(ism);
+
+// let familya = "Berdibekov"
+// console.log(familya);
+
+// let age = 32;
+// console.log(age);
+let a = 5, b = 10, c = "5"
+
+const z = a;
+
+console.log(a == c);
+console.log (a ===c);
+console.log (a == z);
+console.log( a === z);
+console.log (a != b);
